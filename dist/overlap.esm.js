c517644078832d5567b10dba8036df1d48dff969
Mark Mead <itsmarkmead@gmail.com>
2022-12-08 09:18:57 +0000

    Initial commit

---
+++ dist/overlap.esm.js
@@ -0,0 +1,1 @@
+function a(r){r.magic("formData",(t,{})=>Object.fromEntries(new FormData(t))),r.magic("formDataArray",(t,{})=>Object.entries(Object.fromEntries(new FormData(t))))}var m=a;export{m as default};
