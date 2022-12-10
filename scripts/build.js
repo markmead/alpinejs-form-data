a411d00a517548a856da95cd72a9953e8ea6c90f
Mark Mead <itsmarkmead@gmail.com>
2022-12-10 10:31:23 +0000

    Update qoutes

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,11 @@
 buildPlugin({
-  entryPoints: [`builds/cdn.js`],
+  entryPoints: ['builds/cdn.js'],
   outfile: 'dist/form-data.min.js',
 })
 
 buildPlugin({
-  entryPoints: [`builds/module.js`],
-  outfile: `dist/form-data.esm.js`,
+  entryPoints: ['builds/module.js'],
+  outfile: 'dist/form-data.esm.js',
   platform: 'neutral',
   mainFields: ['main', 'module'],
 })
